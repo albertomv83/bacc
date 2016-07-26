ce831d4df2e2d783f64c5f675283188f70e86e82
Alberto Mancheno <amancheno@ip-172-31-22-91.eu-west-1.compute.internal>
2016-07-26 11:57:39 +0000

    Working on more questions

--- 02_read_xlsx_data.R
+++ 02_read_xlsx_data.R
@@ -13,7 +13,7 @@ replace_text_by_options <- function(dt.in,options,column){
 
 obtain_unique_from_multiple <- function(options){
   all <- paste(options[!is.na(options)], collapse=', ')
-  all <- gsub(", ([A-ZAÀÉÈÍÓÒÚÏÜ])","#\\1",all)
+  all <- gsub(", ([A-ZAÃ€Ã‰ÃˆÃÃ“Ã’ÃšÃÃœ])","#\\1",all)
   unique(unlist(strsplit(all, "#")))
 }
 
@@ -23,9 +23,11 @@ bacc <- as.data.table(read.xlsx("respostes.xlsx"))
 question_titles <- gsub("[.]"," ",names(bacc))
 
 #undo option names
+#question 1 simple choice
 r1 <-unique(bacc[[8]])
 r1 <- c(a=r1[2],b=r1[1],c=r1[3],d=r1[4])
 bacc <- replace_text_by_options(bacc,r1,names(bacc)[8])
+#question 2 simple choice
 r2 <- unique(bacc[[9]])
 r2 <- c(a=r2[3],b=r2[2],c=r2[4],d=r2[1])
 bacc <- replace_text_by_options(bacc,r2,names(bacc)[9])
@@ -41,9 +43,23 @@ r4bis <- unique(bacc[[12]])
 r4bis <- obtain_unique_from_multiple(r4bis)
 r4bis <- c(a=r4bis[2],b=r4bis[3],c=r4bis[4],d=r4bis[1])
 bacc <- replace_text_by_options(bacc,r4bis,names(bacc)[12])
-#question 5 is multiple options
+#question 5 is multiple options, changed over time and allowed free text
 r5 <- unique(bacc[[13]])
 r5 <- obtain_unique_from_multiple(r5)
 r5 <- c(a1=r5[5],a2=r5[8],b1=r5[3],b2=r5[6],b3=r5[17],c1=r5[7],c2=r5[1],d=r5[4],e=r5[2])
 bacc <- replace_text_by_options(bacc,r5,names(bacc)[13])
-
+#question 6 is simple options
+r6 <- unique(bacc[[14]])
+r6 <- obtain_unique_from_multiple(r6)
+r6 <- c(a=r6[2],b=r6[6],c=r6[4],d=r6[1],e1=r6[3],e2=r6[5])
+bacc <- replace_text_by_options(bacc,r6,names(bacc)[14])
+#question 7 is simple options
+r7 <- unique(bacc[[15]])
+r7 <- obtain_unique_from_multiple(r7)
+r7 <- c(a1=r7[5],a2=r7[3],b1=r7[2],b2=r7[4],c=r7[1],d=r7[6])
+bacc <- replace_text_by_options(bacc,r7,names(bacc)[15])
+#question 8 is multiple options
+r8 <- unique(bacc[[16]])
+r8 <- obtain_unique_from_multiple(r8)
+r8 <- c(a=r8[3],b=r8[4],c=r8[5],d=r8[1],e1=r8[2],e2=r8[6])
+bacc <- replace_text_by_options(bacc,r8,names(bacc)[16])
